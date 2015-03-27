76321105861c79441666bb916bae3066caf2d2a3
Andres Vega <gydoar@hotmail.com>
2015-03-27 11:56:22 -0500

    Terminando la seccion catering

--- front-page.php
+++ front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<!-- Header -->
 <section id="sec-1">
 	<div class="container">
 		<a id="logo" class="three columns offset-by-one" href="#"><img width="218" src="<?php bloginfo('template_url' ); ?>/library/img/logo.png" alt=""></a>
@@ -10,13 +11,95 @@
 			<p>"EN EL MUNDO DE <strong>LOS EVENTOS <br>NUESTRA <span>EXPERIENCIA </span></strong>ES <br>SU <strong>MEJOR GARANTIA"</strong></p>
 		</div>
 	</div>
-
+	
 	<div class="container">
 		<ul id="menu-header" class="eight columns offset-by-two">
 			<li><a href="">EVENTOS</a></li>
 			<li><a href="">CATERING</a></li>
 			<li><a href="">BTL / AGENCIA</a></li>
 			<li><a href="">CONTACTO</a></li>
+		</ul>
+	</div>
+</section>
+<!-- /Header -->
+
+<!-- Secction 2 -->
+<section id="sec-2" class="container">
+	<div id="w350">
+		<p>Nuestro objetivo es encargarnos de la realización total o parcial de cualquier tipo de<br> evento a nivel <strong>NACIONAL</strong></p>
+		<a href=""><img width="50" src="<?php bloginfo('template_url' ); ?>/library/img/circle.png" alt=""></a>
+	</div>
+</section>
+<!-- /Secction 2 -->
+<!-- Section eventos -->
+<section id="eventos">
+	<div class="container">
+
+		<div class="title" class="twelve columns">
+			<h2>EVENTOS</h2>
+		</div>
+
+			<div id="galeria-1" class="right">
+				<div class="four columns">
+					<img class="twelve columns" src="<?php bloginfo('template_url' ); ?>/library/img/eventos-1.png" alt="">
+					<h3>Empresariales</h3>
+					<p>- Olimpiadas y torneos internos.</p>
+					<p>- Lanzamientos y Activaciones.</p>
+					<p>- Actividades de Bienestar.</p>
+					<p>- Fiestas de fin de año.</p>
+				</div>
+			</div>
+
+			<div id="galeria-1" class="center">
+				<div class="four columns">
+					<img class="twelve columns" src="<?php bloginfo('template_url' ); ?>/library/img/eventos-2.png" alt="">
+					<h3>Recreativos</h3>
+					<p>- Vacaciones Recreativas.</p>
+					<p>- Programas para adultos.</p>
+					<p>- Inflables y Atracciones.</p>
+					<p>- Programas para niños.</p>
+					<p>- Show´s Temáticos.</p>
+				</div>
+			</div>
+
+			<div id="galeria-1" class="left">
+				<div class="four columns">
+					<img class="twelve columns" src="<?php bloginfo('template_url' ); ?>/library/img/eventos-3.png" alt="">
+					<h3>Montajes</h3>
+					<p>- Sonido.</p>
+					<p>- Iluminación.</p>
+					<p>- Video.</p>
+				</div>
+			</div>
+	</div>
+</section>
+<!-- /Section eventos -->
+<!-- Section Catering -->
+<section id="catering">
+	<div class="container">
+		<div class="title" class="twelve columns">
+			<h2>CATERING</h2>
+		</div>
+	</div>
+	
+
+	<div class="container">
+		<div class="four columns">
+			<h3>Alimentos y bebidas</h3>
+			<p>Hemos participado, de manera particular y profesional, en la satisfacción completa de nuestros clientes, ofreciéndoles todas las soluciones disponibles.</p>
+		</div>
+
+		<div class="eight columns">
+			<img class="twelve columns" src="<?php bloginfo('template_url' ); ?>/library/img/catering.png" alt="">
+		</div>
+	</div>
+	<div class="container">
+		<ul id="number">
+			<li><img width="37" src="<?php bloginfo('template_url' ); ?>/library/img/uno.png" alt=""> Menaje y Mobiliario.</li>
+			<li><img width="37" src="<?php bloginfo('template_url' ); ?>/library/img/dos.png" alt=""> Refrigerios.</li>
+			<li><img width="37" src="<?php bloginfo('template_url' ); ?>/library/img/tres.png" alt=""> Desayunos.</li>
+			<li><img width="37" src="<?php bloginfo('template_url' ); ?>/library/img/cuatro.png" alt=""> Almuerzos.</li>
+			<li><img width="37" src="<?php bloginfo('template_url' ); ?>/library/img/cinco.png" alt=""> Cenas.</li>
 		</ul>
 	</div>
 </section>
