ad7946039940dfeb4d8297835b6e382d55b66c5d
Andres Vega <gydoar@hotmail.com>
2015-04-07 16:24:21 -0500

    desactivando ramdon

---
+++ header-home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>
+		<?php
+ 			global $page, $paged; 
+ 			wp_title( '|', true, 'right' ); 
+ 			bloginfo( 'name' ); 
+ 			$site_description = get_bloginfo( 'description', 'display' ); 
+ 				if ( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description"; 
+ 				if ( $paged >= 2 || $page >= 2 ) echo ' | ' . sprintf( __( 'Page %s', 'nakeme' ), max( $paged, $page ) ); 
+ 		?>
+ 	</title>
+ 	<meta name="description" content="<?php bloginfo('description' ); ?>">
+
+ 	<link rel="apple-touch-icon" href="<?php bloginfo('template_url' ); ?>/library/img/apple-touch-icon.png">
+ 	<link rel="icon" type="image/png" href="<?php bloginfo('template_url' ); ?>/library/img/favicon.png">
+
+ 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name') ?> - RSS" href="<?php bloginfo('rss2_url') ?>" />
+    <link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name') ?> - Atom" href="<?php bloginfo('atom_url') ?>" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<!-- css  indispensables -->
+	
+	<link rel="stylesheet" href="<?php bloginfo('template_url' ); ?>/library/css/normalize.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_url' ); ?>/library/css/skeleton.css">
+	<!--/ css indispensables -->
+
+	<!-- css adicionales -->
+
+	<link rel="stylesheet" href="<?php bloginfo('template_url' ); ?>/library/css/owl-carousel.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_url' ); ?>/library/css/owl-theme.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_url' ); ?>/library/css/least.min.css">
+	
+	<!--/ css adicionales -->
+
+	<link rel="stylesheet" href="<?php bloginfo('template_url' ); ?>/style.css">
+
+	<?php wp_head(); ?>
+
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<a id="logo-header" class="three columns offset-by-one" href="<?php echo home_url('/'); ?>"><img width="218" src="<?php bloginfo('template_url' ); ?>/library/img/logo.png" alt=""></a>
+		</div>
+		<div class="container right btn-cont-hedr">
+			<a class="bottom-serv" href="#">CONTACTO</a>
+		</div>
+	</header>
+
+		<ul id="menu-header2" class="center">
+			<li><a href="#eventos">INICIO</a></li>
+			<li><a href="#eventos">EVENTOS</a></li>
+			<li><a href="#catering">CATERING</a></li>
+			<li><a href="#btl">BTL / AGENCIA</a></li>
+			<li><a href="#contactenos">PROYECTOS</a></li>
+		</ul>
---
+++ page-eventos.php
@@ -0,0 +1,101 @@
+<?php get_header("home"); ?>
+	<section id="sub-header" class="center">
+		<div class="container">
+			<div class="title1" class="twelve columns">
+				<h2>EVENTOS EMPRESARIALES</h2>
+			</div>
+			<br>
+			<p>Somos expertos en la creación de eventos personalizados, innovadores, inspirados en los deseos y necesidades  de nuestros clientes. Contamos con un equipo especializado en logística, tecnología, mercadeo y publicidad para hacer de sus eventos corporativos una experiencia de marca única, a la altura de las expectativas de los invitados. </p>
+		</div>
+		<ul id="list__sub">
+			<li><img src="<?php bloginfo('template_url' ); ?>/library/img/line-icon.png" alt=""> &nbsp;Olimpiadas y torneos internos</li>
+			<li><img src="<?php bloginfo('template_url' ); ?>/library/img/line-icon.png" alt=""> &nbsp;Lanzamientos y Actividades</li>
+			<li><img src="<?php bloginfo('template_url' ); ?>/library/img/line-icon.png" alt=""> &nbsp;Actividades de Bienestar</li>
+			<li><img src="<?php bloginfo('template_url' ); ?>/library/img/line-icon.png" alt=""> &nbsp;Fiestas de fin de año</li>
+		</ul>
+		<div class="container" id="caja-gray"></div>
+	</section>
+
+
+		 <!-- Least Gallery -->
+        <section id="least">
+            
+            <!-- Least Gallery: Fullscreen Preview -->
+            <div class="least-preview"></div>
+            
+            <!-- Least Gallery: Thumbnails -->
+            <ul class="least-gallery container">
+                <!-- 1 || Element with data-caption ||-->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/01.jpg" title="Skateboard" data-subtitle="View Picture" data-caption="<strong>Bold text</strong> normal caption text">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/01.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+                
+                <!-- 2 || Element with data-caption as href-attribute ||-->-->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/02.jpg" title="Train Rails" data-subtitle="View Picture" data-caption="Now it's possibe to add an URL-Link into caption text - <a href='http://www.google.com' target='_blank'>GOOGLE</a>">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/02.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+                
+                <!-- 3 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/03.jpg" title="Apple" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/03.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+
+                <!-- 4 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/04.jpg" title="Road Trip" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/04.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+
+                <!-- 5 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/05.jpg" title="Desert" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/05.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+
+                <!-- 6 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/06.jpg" title="Tree" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/06.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+
+                <!-- 7 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/01.jpg" title="MacBook" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/01.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+
+                <!-- 8 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/02.jpg" title="Clock" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/02.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+
+                <!-- 9 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/03.jpg" title="iPhone" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/03.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+
+                <!-- 10 -->
+                <li>
+                    <a href="<?php bloginfo('template_url' ); ?>/library/img/portafolio/04.jpg" title="New York" data-subtitle="View Picture">
+                        <img src="<?php bloginfo('template_url' ); ?>/library/img/portafolio/04.jpg" alt="Alt Image Text" />
+                    </a>
+                </li>
+            </ul>
+
+        </section>
+        <!-- Least Gallery end -->
+<?php get_footer(); ?>
